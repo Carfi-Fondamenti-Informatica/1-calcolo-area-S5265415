005ee0731da7574eb239781721b59ac80e6d4c57
S5265415 <101595525+S5265415@users.noreply.github.com>
2022-03-16 19:24:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+float a, b, c;
+float areatriangolo = ((a * b) / 2), areaquadrato = (a * a), arearettangolo = (a * b), areatrapezio = (((a + b) * c) / 2);
+std::cin >> a >> b >> c;
+std::cout << ((a * b) / 2) << std::endl;
+std::cout << (a * a) << std::endl;
+std::cout << (a * b) << std::endl;
+std::cout << (((a + b) * c) / 2) << std::endl;
 }
